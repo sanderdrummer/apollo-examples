d4fc62998db7a98ccbbce6c222680ef309b2d125
sanderdrummer <tobias.pickel@moia.io>
2023-03-15 16:33:17 +0100

    feat: polling for v7

--- notes-app/packages/notes-frontend/src/app/notes-with-polling/notes-with-polling.tsx
+++ notes-app/packages/notes-frontend/src/app/notes-with-polling/notes-with-polling.tsx
@@ -1,4 +1,4 @@
-// import { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useNotesForPollingQuery } from './notes-with-polling.hook'
 
 export const NotesWithPolling = () => {
@@ -9,9 +9,9 @@ export const NotesWithPolling = () => {
 
   // is fixed with apollo version 3.7 but pollInterval is broken instead xD
   // https://github.com/apollographql/apollo-client/issues/9819
-  // useEffect(() => {
-  // startPolling(2000)
-  // }, [])
+  useEffect(() => {
+    startPolling(2000)
+  }, [startPolling])
 
   return <div>{JSON.stringify(data)}</div>
 }
