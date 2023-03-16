6e5cf7674450ef0ff02bb05863bb97396775b644
sanderdrummer <tobias.pickel@moia.io>
2023-03-16 13:04:31 +0100

    fix: casual

--- notes-app/packages/notes-frontend/src/app/notes-with-polling/notes-with-polling.tsx
+++ notes-app/packages/notes-frontend/src/app/notes-with-polling/notes-with-polling.tsx
@@ -1,4 +1,3 @@
-// import { useEffect } from 'react'
 import { useNotesForPollingQuery } from './notes-with-polling.hook'
 
 export const NotesWithPolling = () => {
@@ -6,12 +5,6 @@ export const NotesWithPolling = () => {
     fetchPolicy: 'no-cache',
     pollInterval: 2000,
   })
-
-  // is fixed with apollo version 3.7 but pollInterval is broken instead xD
-  // https://github.com/apollographql/apollo-client/issues/9819
-  // useEffect(() => {
-  // startPolling(2000)
-  // }, [])
 
   return <div>{JSON.stringify(data)}</div>
 }
